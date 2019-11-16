5edfe3a34acc8520680749767c5869f2f85a2a66
ccandamil <30931926+ccandamil@users.noreply.github.com>
2019-11-16 16:18:30 -0500

    Update view_ex.php

--- view_ex.php
+++ view_ex.php
@@ -1,24 +1,25 @@
-<?php
+<?php 
 
-include("db.php");
+    include("db.php");
 
-if (isset($_GET['examen_id'])) {
-    $examen_id = $_GET['examen_id'];
+    if (isset($_GET['examen_id'])){
+        $examen_id = $_GET['examen_id'];
+        $query = "SELECT tipo FROM examenes WHERE examen_id=$examen_id";
+        $examen_query = mysqli_query($conn, $query);
+        $row = mysqli_fetch_array($examen_query);
+        $tipo = $row['tipo'];
 
-    $query = "SELECT tipo FROM examenes WHERE examen_id=$examen_id";
-    $tipo_query = mysqli_query($conn, $query);
-    $row = mysqli_fetch_array($tipo_query);
-    $tipo = $row['tipo'];
+        if ($tipo="Parcial de Orina"){
+            header("Location: p_orina.php?examen_id=$examen_id");
+        }
 
-    if($tipo="Parcial de Orina"){
-        header("Location: p_orina_view.php?examen_id=$examen_id");
-    }else{
-    $_SESSION['message'] = 'Error Inesperado';
-    $_SESSION['message_type'] = 'danger';
+        if ($tipo="Microbiología"){
+            header("Location: m_bk.php?examen_id=$examen_id");
+        }
 
-    header("Location: pacientes.php");
-    die();
+        if ($tipo="Hematología"){
+            header("Location: h_hematologia.php?examen_id=$examen_id");
+        }
     }
-    
-}
+
 ?>
