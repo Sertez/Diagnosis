9ae616839fa22d04e6c0495ffd68ba8b21cc8827
ccandamil <30931926+ccandamil@users.noreply.github.com>
2019-11-16 22:15:55 -0500

    Update h_hematologia_view.php

--- hema/h_hematologia_view.php
+++ hema/h_hematologia_view.php
@@ -20,7 +20,7 @@ function calculaedad($fechanacimiento)
 
 <?php if (isset($_GET['examen_id'])) { ?>
     <div class="container p-4 con" id="contenido">
-        <div class="card card-body">
+        <div class="card card-body" id="card">
             <div class="card-header" id="extitle">
                 Hematología
             </div>
@@ -28,13 +28,13 @@ function calculaedad($fechanacimiento)
                 $examen_id = $_GET['examen_id'];
                 ?>
             <?php
-                $query = "SELECT * FROM examenes WHERE examen_id=$examen_id";
+                $query = "SELECT * FROM examenes WHERE examen_id='$examen_id'";
                 $ex_query = mysqli_query($conn, $query);
                 $row = mysqli_fetch_array($ex_query);
                 $paciente_id = $row['paciente_id'];
                 $tipo = $row['tipo'];
                 $fecha = $row['fecha'];
-                $query = "SELECT * FROM pacientes WHERE paciente_id=$paciente_id";
+                $query = "SELECT * FROM pacientes WHERE paciente_id='$paciente_id'";
                 $pac_query = mysqli_query($conn, $query);
                 $row = mysqli_fetch_array($pac_query);
                 $eps = $row['eps'];
@@ -44,28 +44,26 @@ function calculaedad($fechanacimiento)
                 $edad = calculaedad($fechanac);
                 ?>
             <?php
-                $query = "SELECT * FROM h_hematologia WHERE examen_id=$examen_id";
+                $query = "SELECT * FROM h_hematologia WHERE examen_id='$examen_id'";
                 $ex_query = mysqli_query($conn, $query);
                 $row = mysqli_fetch_array($ex_query);
-                $color = $row['color'];
-                $aspecto = $row['aspecto'];
-                $ph = $row['ph'];
-                $densidad = $row['densidad'];
-                $proteinas = $row['proteinas'];
-                $glucosa = $row['glucosa'];
-                $cetonas = $row['cetonas'];
-                $sangre = $row['sangre'];
-                $bilirrubinas = $row['bilirrubinas'];
-                $urobilinogeno = $row['urobilinogeno'];
-                $nitritos = $row['nitritos'];
-                $cel_epiteliales = $row['cel_epiteliales'];
-                $moco = $row['moco'];
-                $bacterias = $row['bacterias'];
-                $leucocitos = $row['leucocitos'];
-                $piocitos = $row['piocitos'];
-                $hematies = $row['hematies'];
-                $cristales = $row['cristales'];
-                $cilindros = $row['cilindros'];
+                $hematocrito= $row['hematocrito'];
+                $hemoglobina= $row['hemoglobina'];   
+                $leucocitos= $row['leucocitos']; 
+                $neutrofilos= $row['neutrofilos'];
+                $linfocitos= $row['linfocitos'];
+                $eosinofilos= $row['eosinofilos'];
+                $monocitos= $row['monocitos'];
+                $basofilas= $row['basofilas'];
+                $plaquetas= $row['plaquetas'];
+                $reticulocitos= $row['reticulocitos'];
+                $vsg= $row['vsg'];
+                $t_sangria= $row['t_sangria'];
+                $t_coagulacion= $row['t_coagulacion'];
+                $t_protrombina= $row['t_protrombina'];    
+                $t_p_tromboplastina= $row['t_p_tromboplastina'];
+                $grupo= $row['grupo'];
+                $rh= $row['rh'];
                 $bacteriologo = $row['bacteriologo'];
                 ?>
             <div class="card-body">
@@ -100,101 +98,21 @@ function calculaedad($fechanacimiento)
                 <div class="col-sm-6">
                     <div class="card" id="formulario">
                         <div class="card-header" id="cardtitle">
-                            Examen Físico
+                            Cuadro Hemático
                         </div>
                         <div class="card-body">
                             <div class="form-group row">
-                                <label for="color" class="col-sm-6 col-form-label">Color</label>
+                                <label for="hematocrito" class="col-sm-6 col-form-label">Hematocrito</label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="color" class="form-control" value="<?php echo $color ?>" readonly>
+                                    <input type="text" name="hematocrito" class="form-control" value="<?php echo $hematocrito ?>" readonly>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="aspecto" class="col-sm-6 col-form-label">Aspecto</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="aspecto" class="form-control" value="<?php echo $aspecto ?>" readonly>
+                                <label for="hemoglobina" class="col-sm-4 col-form-label">Hemoglobina</label>
+                                <div class="col-sm-4">
+                                    <input type="text" name="hemoglobina" class="form-control" value="<?php echo $hemoglobina ?>" readonly>                                    
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="ph" class="col-sm-6 col-form-label">PH</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="ph" class="form-control" value="<?php echo $ph ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="densidad" class="col-sm-6 col-form-label">Densidad</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="densidad" class="form-control" value="<?php echo $densidad ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="proteinas" class="col-sm-6 col-form-label">Proteinas</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="proteinas" class="form-control" value="<?php echo $proteinas ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="glucosa" class="col-sm-6 col-form-label">Glucosa</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="glucosa" class="form-control" value="<?php echo $glucosa ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="cetonas" class="col-sm-6 col-form-label">Cetonas</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="cetonas" class="form-control" value="<?php echo $cetonas ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="sangre" class="col-sm-6 col-form-label">Sangre</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="sangre" class="form-control" value="<?php echo $sangre ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="bilirrubinas" class="col-sm-6 col-form-label">Bilirrubinas</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="bilirrubinas" class="form-control" value="<?php echo $bilirrubinas ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="urobilinogeno" class="col-sm-6 col-form-label">Urobilinogeno</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="urobilinogeno" class="form-control" value="<?php echo $urobilinogeno ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="nitritos" class="col-sm-6 col-form-label">Nitritos</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="nitritos" class="form-control" value="<?php echo $nitritos ?>" readonly>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card" id="formulario">
-                        <div class="card-header" id="cardtitle">
-                            Examen Microscópico
-                        </div>
-                        <div class="card-body">
-                            <div class="form-group row">
-                                <label for="cel_epiteliales" class="col-sm-6 col-form-label">Celulas Epiteliales</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="cel_epiteliales" class="form-control" value="<?php echo $cel_epiteliales ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="moco" class="col-sm-6 col-form-label">Moco</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="moco" class="form-control" value="<?php echo $moco ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="bacterias" class="col-sm-6 col-form-label">Bacterias</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="bacterias" class="form-control" value="<?php echo $bacterias ?>" readonly>
-                                </div>
+                                <label for="ref" class="col-sm-4 col-form-label">M:12-14; H:14-16gr/dl</label>
                             </div>
                             <div class="form-group row">
                                 <label for="leucocitos" class="col-sm-6 col-form-label">Leucocitos</label>
@@ -203,27 +121,91 @@ function calculaedad($fechanacimiento)
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="piocitos" class="col-sm-6 col-form-label">Piocitos</label>
+                                <label for="neutrofilos" class="col-sm-6 col-form-label">Neutrofilos</label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="piocitos" class="form-control" value="<?php echo $piocitos ?>" readonly>
+                                    <input type="text" name="neutrofilos" class="form-control" value="<?php echo $neutrofilos ?>" readonly>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="hematies" class="col-sm-6 col-form-label">Hematies</label>
+                                <label for="linfocitos" class="col-sm-6 col-form-label">Linfocitos</label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="hematies" class="form-control" value="<?php echo $hematies ?>" readonly>
+                                    <input type="text" name="linfocitos" class="form-control" value="<?php echo $linfocitos ?>" readonly>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="cristales" class="col-sm-6 col-form-label">Cristales</label>
+                                <label for="eosinofilos" class="col-sm-6 col-form-label">Eosinofilos</label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="cristales" class="form-control" value="<?php echo $cristales ?>" readonly>
+                                    <input type="text" name="eosinofilos" class="form-control" value="<?php echo $eosinofilos ?>" readonly>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="cilindros" class="col-sm-6 col-form-label">Cilindros</label>
+                                <label for="monocitos" class="col-sm-6 col-form-label"> Monocitos</label>
+                                <div class="col-sm-6">  
+                                    <input type="text" name="monocitos" class="form-control" value="<?php echo $monocitos ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="basofilas" class="col-sm-6 col-form-label">Basofilas</label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="cilindros" class="form-control" value="<?php echo $cilindros ?>" readonly>
+                                    <input type="text" name="basofilas" class="form-control" value="<?php echo $basofilas ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="plaquetas" class="col-sm-6 col-form-label">Plaquetas</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="plaquetas" class="form-control" value="<?php echo $plaquetas ?>" readonly>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                                
+                <div class="col-sm-6">
+                    <div class="card" id="formulario">
+                        <div class="card-header" id="cardtitle">
+                            Otros Exámenes
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="reticulocitos" class="col-sm-6 col-form-label">Reticulocitos</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="reticulocitos" class="form-control" value="<?php echo $reticulocitos ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="vsg" class="col-sm-6 col-form-label">V.S.G</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="vsg" class="form-control" value="<?php echo $vsg ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="t_sangria" class="col-sm-6 col-form-label">Tiempo de Sangría</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="t_sangria" class="form-control" value="<?php echo $t_sangria ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="t_protombina" class="col-sm-6 col-form-label">Tiempo de Protombina  </label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="t_protombina" class="form-control" value="<?php echo $t_protrombina ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="t_p_tromboplastina" class="col-sm-6 col-form-label">Tiempo Parcial de Tromboplastina</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="t_p_tromboplastina" class="form-control" value="<?php echo $t_p_tromboplastina ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="grupo" class="col-sm-6 col-form-label">Grupo Sanguíneo</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="grupo" class="form-control" value="<?php echo $grupo ?>" readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="rh" class="col-sm-6 col-form-label">Factor RH</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="rh" class="form-control" value="<?php echo $rh ?>" readonly>
                                 </div>
                             </div>
                         </div>
@@ -232,7 +214,7 @@ function calculaedad($fechanacimiento)
             </div>
             <div class="form-group mb-2 row" id="bac_space">
                 <label for="bacteriologo" class="col-form-label">Bacteriologo</label>
-                <input type="text" name="cilindros" class="form-control" value="<?php echo $bacteriologo ?>" readonly>
+                <input type="text" name="rh" class="form-control" value="<?php echo $bacteriologo ?>" readonly>
             </div>
         </div>
     </div>
