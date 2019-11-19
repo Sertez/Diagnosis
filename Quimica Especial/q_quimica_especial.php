0ee2efe02eff56c66dcc80de41d4cd91d4f7615e
Giovanoti <30931918+Giovanoti@users.noreply.github.com>
2019-11-19 16:56:49 -0500

    Add files via upload

---
+++ Quimica Especial/q_quimica_especial.php
@@ -0,0 +1,92 @@
+<?php include("db.php"); ?>
+<?php include("includes/header.php"); ?>
+<?php if (isset($_GET['examen_id'])) {
+    $id1 = $_GET['examen_id'];
+    $hemoglobina1 = "";
+    $microalbuminura1 = "";
+    $creatinuria1 = "";
+    $albumi_creati1 = "";
+    $bacteriologo1 = "";
+    $query = "SELECT * FROM q_quimica_especial WHERE examen_id='$id1'";
+    $result2 = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result2) == 1) {
+        $row = mysqli_fetch_array($result2);
+        $hemoglobina1 = $row['hemoglobina'];
+        $microalbuminura1 = $row['microalbuminura'];
+        $creatinuria1 = $row['creatinuria'];
+        $albumi_creati1 = $row['albumi_creati'];
+        $bacteriologo1 = $row['bacteriologo'];
+    }
+    ?>
+
+    <div class="container p-4 con" id="contenido">
+        <form action="q_quimica_especial_save.php?examen_id=<?php echo $_GET['examen_id'] ?>" method="POST">
+            <div class="card card-body">
+                <div class="card-header" id="extitle">
+                    Quimica Especial
+                </div>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <div class="card card-body" id="center">
+                            <div class="card-header" id="cardtitle">
+                                Quimica
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group row">
+                                    <label for="hemoglobina" class="col-sm-4 col-form-label">Hemoglobina</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="hemoglobina" value="<?php echo $hemoglobina1 ?>" class="form-control" placeholder="" autofocus>
+                                    </div>
+                                    <label for="ref" class="col-sm-4 col-form-label">4.4-6.7% NO DIABETICO <br> 6.7-7.3% OBJETIVO <br> 7.3-9.1% BUEN CONTROL</label>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="microalbuminura" class="col-sm-4 col-form-label">Microalbuminura</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="microalbuminura" value="<?php echo $microalbuminura1 ?>" class="form-control" placeholder="" autofocus>
+                                    </div>
+                                    <label for="ref" class="col-sm-4 col-form-label">< 20 mg/L </label> 
+                                </div>
+                                <div class="form-group row">
+                                    <label for="creatinuria" class="col-sm-4 col-form-label">Creatinuria</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="creatinuria" value="<?php echo $creatinuria1 ?>" class="form-control" placeholder="" autofocus>
+                                    </div>
+                                    <label for="ref" class="col-sm-4 col-form-label">39-259 mg/dl</label>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="albumi_creati" class="col-sm-4 col-form-label">Albumi Creati</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="albumi_creati" value="<?php echo $albumi_creati1 ?>" class="form-control" placeholder="" autofocus>
+                                    </div>
+                                    <label for="ref" class="col-sm-4 col-form-label">< 30 mg/L </label> 
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div>
+                </div>
+                    <div class="form-group mb-2 row" id="bac_space">
+                        <label for="bacteriologo" class="col-form-label">Bacteriologo</label>
+                        <div class="mx-sm-4 mb-2">
+                            <select class="form-control" id="sel_width" name="bacteriologo">
+                                <option selected="true" disabled="disabled" value="<?php echo $bacteriologo1 ?>"><?php echo $bacteriologo1 ?></option>
+                                <?php
+                                    $query = "SELECT * FROM bacteriologos";
+                                    $bac_table = mysqli_query($conn, $query);
+                                    while ($row = mysqli_fetch_array($bac_table)) { ?>
+                                    <option value="<?php echo $row['nombre'] ?>"><?php echo $row['nombre'] ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="q_quimica_especial_save" value="Agregar o Editar Examen">
+                </div>
+            </form>
+    </div>
+<?php } else {
+    header("Location:examenes.php");
+    die();
+}
+?>
+
+
+<?php include("includes/footer.php"); ?>
---
+++ Quimica Especial/q_quimica_especial_view.php
@@ -0,0 +1,136 @@
+<?php include("db.php"); ?>
+<?php include("includes/header.php"); ?>
+
+<?php
+function calculaedad($fechanacimiento)
+{
+    list($ano, $mes, $dia) = explode("-", $fechanacimiento);
+    $ano_diferencia  = date("Y") - $ano;
+    $mes_diferencia = date("m") - $mes;
+    $dia_diferencia   = date("d") - $dia;
+    if ($mes_diferencia < 0) {
+        $ano_diferencia--;
+    } else {
+        if ($mes_diferencia == 0 and $dia_diferencia < 0)
+            $ano_diferencia--;
+    }
+    return $ano_diferencia;
+}
+?>
+
+<?php if (isset($_GET['examen_id'])) { ?>
+    <div class="container p-4 con" id="contenido">
+        <div class="card card-body">
+            <div class="card-header" id="extitle">
+                Quimica Especial
+            </div>
+            <?php
+                $examen_id = $_GET['examen_id'];
+                ?>
+            <?php
+                $query = "SELECT * FROM examenes WHERE examen_id=$examen_id";
+                $ex_query = mysqli_query($conn, $query);
+                $row = mysqli_fetch_array($ex_query);
+                $paciente_id = $row['paciente_id'];
+                $tipo = $row['tipo'];
+                $fecha = $row['fecha'];
+                $query = "SELECT * FROM pacientes WHERE paciente_id=$paciente_id";
+                $pac_query = mysqli_query($conn, $query);
+                $row = mysqli_fetch_array($pac_query);
+                $eps = $row['eps'];
+                $nombre = $row['nombre'];
+                $doc = $row['identificacion'];
+                $fechanac = $row['fechanac'];
+                $edad = calculaedad($fechanac);
+                ?>
+            <?php
+                $query = "SELECT * FROM q_quimica_especial WHERE examen_id=$examen_id";
+                $ex_query = mysqli_query($conn, $query);
+                $row = mysqli_fetch_array($ex_query);
+                $hemoglobina = $row['hemoglobina'];
+                $microalbuminura = $row['microalbuminura'];
+                $creatinuria = $row['creatinuria'];
+                $albumi_creati = $row['albumi_creati'];
+                $bacteriologo = $row['bacteriologo'];
+                ?>
+            <div class="card-body">
+                <div class="card card-body">
+                    <div class="row">
+                        <div class="col">
+                            <label for="patient-name">Nombres y Apellidos</label>
+                            <input type="text" name="pname" class="form-control" value="<?php echo $nombre ?>" readonly>
+                        </div>
+                        <div class="col">
+                            <label for="document">Documento</label>
+                            <input type="int" name="doc" class="form-control" value="<?php echo $doc ?>" readonly>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label for="eps">EPS</label>
+                            <input type="text" name="eps" class="form-control" value="<?php echo $eps ?>" readonly>
+                        </div>
+                        <div class="col">
+                            <label for="date">Fecha</label>
+                            <input type="date" name="date" class="form-control" value="<?php echo $fecha ?>" readonly>
+                        </div>
+                        <div class="col">
+                            <label for="age">Edad</label>
+                            <input type="int" name="edad" class="form-control" value="<?php echo $edad ?>" readonly>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="card" id="formulario">
+                        <div class="card-header" id="cardtitle">
+                            Quimica
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="hemoglobina" class="col-sm-4 col-form-label">Hemoglobina</label>
+                                <div class="col-sm-4">
+                                    <input type="text" name="hemoglobina" class="form-control" value="<?php echo $hemoglobina ?>" readonly>
+                                </div>
+                                <label for="ref" class="col-sm-4 col-form-label">4.4-6.7% NO DIABETICO <br> 6.7-7.3% OBJETIVO <br> 7.3-9.1% BUEN CONTROL</label>
+                            </div>
+                            <div class="form-group row">
+                                <label for="microalbuminura" class="col-sm-4 col-form-label">microalbuminura</label>
+                                <div class="col-sm-4">
+                                    <input type="text" name="microalbuminura" class="form-control" value="<?php echo $microalbuminura ?>" readonly>
+                                </div>
+                                <label for="ref" class="col-sm-4 col-form-label">< 20 mg/L </label> 
+                            </div> 
+                            <div class="form-group row">
+                                <label for="creatinuria" class="col-sm-4 col-form-label">Creatinuria</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="creatinuria" class="form-control" value="<?php echo $creatinuria ?>" readonly>
+                                    </div>
+                                    <label for="ref" class="col-sm-4 col-form-label">39-259 mg/dl</label>
+                            </div>
+                            <div class="form-group row">
+                                <label for="albumi_creati" class="col-sm-4 col-form-label">albumi_creati</label>
+                                    <div class="col-sm-4"> 
+                                        <input type="text" name="albumi_creati" class="form-control" value="<?php echo $albumi_creati ?>" readonly>
+                                    </div>
+                                <label for="ref" class="col-sm-4 col-form-label">< 30 mg/L </label>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group mb-2 row" id="bac_space">
+               <label for="bacteriologo" class="col-form-label">Bacteriologo</label>
+                    <input type="text" name="bacteriologo" class="form-control" value="<?php echo $bacteriologo ?>" readonly>
+            </div>
+        </div>
+   </div>
+   <?php } else {
+     $_SESSION['message'] = 'Error Inesperado';
+     $_SESSION['message_type'] = 'danger';
+    header("Location:pacientes.php");
+    die();
+  }
+   ?>
+  <?php include("includes/footer.php"); ?>
