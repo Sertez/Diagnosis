65668b75062daa065ef0f5be3cf8c1da866199be
Dulco <30907887+Dulco@users.noreply.github.com>
2019-11-21 16:32:43 -0500

    Add files via upload

--- save_ex.php
+++ save_ex.php
@@ -4,13 +4,17 @@ include("db.php");
 
 if (isset($_POST['save_ex'])) {
     $p_doc = $_POST['doc'];
-    $tipo = $_POST['tipo'];
-
     $query = "SELECT paciente_id FROM pacientes WHERE identificacion=$p_doc";
     $paciente_query = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($paciente_query);
     $paciente_id = $row['paciente_id'];
-
+    if($paciente_id==""){
+        $_SESSION['message'] = 'Digite un documento válido';
+        $_SESSION['message_type'] = 'danger';
+        header("Location: examenes.php");
+        die();
+    }
+    $tipo = $_POST['tipo'];
     $query = "INSERT INTO examenes(paciente_id,doc_paciente,tipo) 
     VALUES('$paciente_id','$p_doc','$tipo')";
     $result = mysqli_query($conn, $query);
@@ -20,6 +24,7 @@ if (isset($_POST['save_ex'])) {
         header("Location: examenes.php");
         die();
     }
+    
 
     $_SESSION['message'] = 'Examen creado con éxito';
     $_SESSION['message_type'] = 'success';
