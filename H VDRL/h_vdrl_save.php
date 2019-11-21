ea7fa733fba8d758d840ce9b23810c54c489cb17
Giovanoti <30931918+Giovanoti@users.noreply.github.com>
2019-11-21 00:03:37 -0500

    Add files via upload

---
+++ H VDRL/h_vdrl_save.php
@@ -0,0 +1,29 @@
+<?php
+
+include("db.php");
+
+if ((isset($_POST['h_vdrl_save'])) and (isset($_GET['examen_id']))) {
+    $examen_id = $_GET['examen_id'];
+    $vdrl = $_POST['vdrl'];
+    $bacteriologo = $_POST['bacteriologo'];
+    $query2 = "SELECT * FROM h_vdrl WHERE examen_id='$examen_id'";
+    $result5 = $result = mysqli_query($conn, $query2);
+    $row = mysqli_fetch_array($result5);
+    if ($row['examen_id'] == $examen_id) {
+        $query3 = "UPDATE h_vdrl SET vdrl = '$vdrl', 
+        bacteriologo='$bacteriologo' WHERE examen_id='$examen_id'";
+    } else {
+        $query = "INSERT INTO h_vdrl(examen_id,vdrl,bacteriologo) 
+            VALUES('$examen_id','$vdrl','$bacteriologo')";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            $_SESSION['message'] = 'Error Inesperado';
+            $_SESSION['message_type'] = 'danger';
+            header("Location: examenes.php");
+            die();
+        }
+    }
+    $_SESSION['message'] = 'Operacion relizada con éxito';
+    $_SESSION['message_type'] = 'success';
+    header("Location: examenes.php");
+}
