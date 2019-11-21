6f0fd6418373411d5bc4e2e513200cee5dec7b05
ccandamil <30931926+ccandamil@users.noreply.github.com>
2019-11-21 14:31:18 -0500

    Update del_ex.php

--- del_ex.php
+++ del_ex.php
@@ -9,46 +9,70 @@ if (isset($_GET['examen_id'])) {
     $tipo_query = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($tipo_query);
     $tipo = $row['tipo'];
-
-    $query = "DELETE FROM examenes WHERE examen_id = $examen_id";
-    $result=mysqli_query($conn,$query);
+    $result2= "";
 
     if($tipo="Parcial de Orina"){
         $query = "DELETE FROM p_orina WHERE examen_id = $examen_id";
-        $result=mysqli_query($conn,$query);
+        $result2=mysqli_query($conn,$query);
         header("Location: examenes.php");
-        die();
     }
     if ($tipo="Microbiología"){
         $query = "DELETE FROM m_bk WHERE examen_id = $examen_id";
-        $result=mysqli_query($conn,$query);
+        $result2=mysqli_query($conn,$query);
         header("Location: examenes.php");
-        die();
     }
     if ($tipo="Hematología"){
         $query = "DELETE FROM h_hematologia WHERE examen_id = $examen_id";
-        $result=mysqli_query($conn,$query);
+        $result2=mysqli_query($conn,$query);
         header("Location: examenes.php");
-        die();
     }
     if ($tipo="Urocultivo"){
         $query = "DELETE FROM m_uro_positivo WHERE examen_id = $examen_id";
-        $result=mysqli_query($conn,$query);
+        $result2=mysqli_query($conn,$query);
         header("Location: examenes.php");
-        die();
     }    
     if ($tipo="Inmunología"){
         $query = "DELETE FROM i_gravindex WHERE examen_id = $examen_id";
-        $result=mysqli_query($conn,$query);
+        $result2=mysqli_query($conn,$query);
         header("Location: examenes.php");
-        die();
     }
-
-    $_SESSION['message'] = 'Error Inesperado';
-    $_SESSION['message_type'] = 'danger';
-
+    if ($tipo="Química"){
+        $query = "DELETE FROM q_quimica WHERE examen_id = $examen_id";
+        $result2=mysqli_query($conn,$query);
+        header("Location: examenes.php");
+        
+    }
+    if ($tipo="PT PTT"){
+        $query = "DELETE FROM h_pt_ptt WHERE examen_id = $examen_id";
+        $result2=mysqli_query($conn,$query);
+        header("Location: examenes.php");
+    }
+    if ($tipo="Ionograma"){
+        $query = "DELETE FROM q_ionograma WHERE examen_id = $examen_id";
+        $result2=mysqli_query($conn,$query);
+        header("Location: examenes.php");
+    }
+    if ($tipo="HTO HB"){
+        $query = "DELETE FROM h_hto_hb WHERE examen_id = $examen_id";
+        $result2=mysqli_query($conn,$query);
+        header("Location: examenes.php");
+    }
+    if ($tipo="UrocultivoN"){
+        $query = "DELETE FROM m_uro_negativo WHERE examen_id = $examen_id";
+        $result2=mysqli_query($conn,$query);
+        header("Location: examenes.php");
+    }
+    if($result2=""){        
+        $_SESSION['message'] = 'Error Inesperado';
+        $_SESSION['message_type'] = 'danger';
+    }else{
+        $query = "DELETE FROM examenes WHERE examen_id = $examen_id";
+        $result=mysqli_query($conn,$query);
+        $_SESSION['message'] = 'Eliminación Completa';
+        $_SESSION['message_type'] = 'success';
+    }
     header("Location: examenes.php");
     die();
-    
+
 }
 ?>
